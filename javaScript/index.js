66acfd8aca5d99d007114c4af285bdd9062ed540
MudassirShah1335 <MudassirShah1335@gmail.com>
2023-10-02 14:35:14 +0500

    namawrka

---
+++ javaScript/index.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
